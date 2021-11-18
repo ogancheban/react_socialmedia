7078b380ddd47cde0592092d8563de04c9675ec4
ogancheban <e.tchebanov@gmail.com>
2021-11-18 23:50:09 +0300

    switch nav color

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,40 +1,42 @@
 import React from 'react'
 import s from './Navbar.module.css'
-import { Link } from "react-router-dom";
+import { NavLink, activeClassName } from 'react-router-dom';
+
 
 const Navbar = () => {
     return (
+
         <nav className={`${s.sidebar__menu} ${s.menu}`}>
             <ul className={s.menu__list}>
                 <li className={s.menu__item}>
                     <div className={s.menu__icon}>
-                        <img src="./img/menu icon/menu_01.svg" alt="Profile" />
+                        <img src="./img/menu icon/menu_01.svg" alt="Profile"/>
                     </div>
-                    <Link to='/profile' activeClassName={s.activeLink} className={s.menu__link} >Profile</Link>
+                    <NavLink exact to='/profile' style={({ isActive }) =>({color: isActive ? 'blue' : ' #34305c'})} className={s.menu__link}>Profile</NavLink>
                 </li>
                 <li className={s.menu__item}>
                     <div className={s.menu__icon}>
-                        <img src="./img/menu icon/menu_02.svg" alt="Profile" />
+                        <img src="./img/menu icon/menu_02.svg" alt="Profile"/>
                     </div>
-                    <Link to="/dialogs" className={s.menu__link}>Messages</Link>
+                    <NavLink end to="/dialogs" style={({ isActive }) =>({color: isActive ? 'blue' : ' #34305c'})} className={s.menu__link}>Messages</NavLink>
                 </li>
                 <li className={s.menu__item}>
                     <div className={s.menu__icon}>
-                        <img src="./img/menu icon/menu_03.svg" alt="Profile" />
+                        <img src="./img/menu icon/menu_03.svg" alt="Profile"/>
                     </div>
-                    <Link to="/news" className={s.menu__link}>News</Link>
+                    <NavLink to="/news" className={s.menu__link}>News</NavLink>
                 </li>
                 <li className={s.menu__item}>
                     <div className={s.menu__icon}>
-                        <img src="./img/menu icon/menu_04.svg" alt="Profile" />
+                        <img src="./img/menu icon/menu_04.svg" alt="Profile"/>
                     </div>
-                    <Link to="/music" className={s.menu__link}>Music</Link>
+                    <NavLink to="/music" className={s.menu__link}>Music</NavLink>
                 </li>
-                <li className={`${s.menu__item} ${s._item_last }`}>
+                <li className={`${s.menu__item} ${s._item_last}`}>
                     <div className={s.menu__icon}>
-                        <img src="./img/menu icon/menu_05.svg" alt="Profile" />
+                        <img src="./img/menu icon/menu_05.svg" alt="Profile"/>
                     </div>
-                    <Link to="/settings" className={s.menu__link}>Settings</Link>
+                    <NavLink to="/settings" className={s.menu__link}>Settings</NavLink>
                 </li>
             </ul>
         </nav>
